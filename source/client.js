ef67a1d00f34cd38fdddf092146d7e5ec7d97979
VahanAper <asryanvahandev@gmail.com>
2017-06-16 15:21:59 +0400

    hot reloading enabled

---
+++ source/client.js
@@ -0,0 +1,1 @@
+console.log('from clinet.js2');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 module.exports = {
+  entry: './source/client.js',
+  output: {
+    path: './',
+    filename: 'index.js',
+  },
   devServer: {
-    // inline: true,
+    inline: true,
     contentBase: './public',
     port: 3000,
   }
