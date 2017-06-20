e0551229aaa487350a5d5394dfb6faec889b7fe1
VahanAper <asryanvahandev@gmail.com>
2017-06-20 14:26:03 +0400

    animations add-on installed and initial animation setup

--- source/app.js
+++ source/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import Jumbotron from './bootstrap/jumbotron';
 import Button from './bootstrap/button';
@@ -7,28 +8,28 @@ import './appsource.scss';
 export default class App extends React.Component {
   render() {
     return (
-      <Jumbotron
-        className="-fluid text-xs-center custom"
-        containerFluid={true}
+      <ReactCSSTransitionGroup
+        transitionName="app"
+        transitionAppear={true}
+        transitionAppearTimout={500}
       >
-        <h1>Building React user interfces</h1>
-        <p>with bootstrap and SASS</p>
-        <p>
-          <Button
-            className="-primary"
-            href="http://02geek.com"
-            target="_blank"
-            disabled
-          />
-          <Button
-            className="-danger-outline -sm"
-            disabled
-          />
-          <Button
-            className="-primary-outline -lg -block"
-          />
-        </p>
-      </Jumbotron>
+        <Jumbotron
+          className="-fluid custom"
+          containerFluid={true}
+        >
+          <h1>Building React user interfces</h1>
+          <p>with bootstrap and SASS</p>
+          <p>
+            <Button
+              className="-primary"
+              href="http://02geek.com"
+              target="_blank"
+            />
+            <Button className="-danger-outline -sm" />
+            <Button className="-primary-outline -lg -block" />
+          </p>
+        </Jumbotron>
+      </ReactCSSTransitionGroup>
     );
   }
 }
